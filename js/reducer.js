279ae5b01de74f25e5601f5940a689881bcf2973
agdsarge <agdsargeant@gmail.com>
2020-07-13 11:35:30 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,7 @@
+
+let state = {count: 0}
+let action = {type: 'INCREASE_COUNT'}
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +11,19 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+
+
 
 changeState(state, action)
+
+function dispatch(action) {
+    state = changeState(state, action)
+    render()
+    // return state
+}
+
+function render(){
+  document.body.textContent = state.count
+}
+
+render()
